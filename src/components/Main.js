2a107559e1d0c50a7d54432ad294b925fc218f35
Diogo Azevedo <diogoazevedos@gmail.com>
2015-07-06 14:56:29 -0300

    Feat: Add parse config and initializer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import { RouteHandler } from 'react-router'
+import { Parse } from 'parse'
+import { Application, JavaScript } from '../config/parse'
+
+Parse.initialize(Application, JavaScript)
 
 class Main extends Component {
   render() {
---
+++ src/config/parse.js
@@ -0,0 +1,4 @@
+export default {
+  Application: 'r2V0iIRgO6tvJs7ggvM2iHJ1g342S0JeBkf26g85',
+  JavaScript: 'cxsylcv1rviGEURroktgdTaaTG6lI5GIIy8pSggQ'
+}
